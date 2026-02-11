e3de29951bbec19b6be77807024c8ae75117d9b8
Mikel <mikel.san@educa.madrid.org>
2026-02-11 10:23:52 +0100

    findBooksByGenre

--- src/main/java/org/ies/tierno/model/Library.java
+++ src/main/java/org/ies/tierno/model/Library.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.ies.tierno.Main;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeSet;
@@ -15,4 +16,14 @@ public class Library {
     private Map<String, Book> booksByIsbn;
     private List<Customer> customers;
     private TreeSet<BookLend> bookLends;
+
+    public List<Book> findBooksByGenre(String genre) {
+        List<Book> genreBooks = new ArrayList<>();
+        for (Book book : booksByIsbn.values()) {
+            if (book.getGenres().contains(genre)) {
+                genreBooks.add(book);
+            }
+        }
+        return genreBooks;
+    }
 }
